47ed51b5c959f5f42197a30abd587d0b9cfd8966
Filip Chalupa <chalupa.filip@gmail.com>
2023-08-05 23:39:16 +0200

    Add description and og image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,16 @@ export default function App({ Component, pageProps }: AppProps) {
 			<CssBaseline />
 			<Head>
 				<title>Klávesnice</title>
+				<meta
+					name="description"
+					content="Přehled užitečných počítačových kláves a znázornění, jak je napsat."
+				/>
+				<meta property="og:image:width" content="1200" />
+				<meta property="og:image:height" content="630" />
+				<meta
+					property="og:image"
+					content="https://klavesnice.czechitas-podklady.cz/og-image.jpg"
+				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<Component {...pageProps} />
